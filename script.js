06053fe8cb916805bc4a3df09ddfe12436370d07
mojitopablito <144337928+mojitopablito@users.noreply.github.com>
2024-04-11 09:55:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,6 @@
-// Skrypt do obliczeń
-function calculate() {
-  // Pobranie wartości z pól tekstowych
-  const inputValue = parseFloat(document.getElementById("inputVariable").value);
-  const secondValue = parseFloat(
-    document.getElementById("secondVariable").value
-  );
-
-  // Obliczenia
-  const additionResult = inputValue + secondValue;
-  const subtractionResult = inputValue - secondValue;
-  const multiplicationResult = inputValue * secondValue;
-  const divisionResult = inputValue / secondValue;
-
-  // Wyświetlenie wyników w divach
-  document.getElementById(
-    "additionResult"
-  ).innerText = `Dodawanie: ${additionResult}`;
-  document.getElementById(
-    "subtractionResult"
-  ).innerText = `Odejmowanie: ${subtractionResult}`;
-  document.getElementById(
-    "multiplicationResult"
-  ).innerText = `Mnożenie: ${multiplicationResult}`;
-  document.getElementById(
-    "divisionResult"
-  ).innerText = `Dzielenie: ${divisionResult}`;
+// Skrypt do mnożenia przez 2
+function multiplyByTwo() {
+    const inputValue = parseFloat(document.getElementById('inputVariable').value);
+    const result = inputValue * 2;
+    document.getElementById('multiplicationResult').innerText = `Wynik mnożenia przez 2: ${result}`;
 }
